08a4e7323cbb6e64459275685d30a3d658e7a319
webbyeme <77994619+BigDiaz@users.noreply.github.com>
2025-12-11 17:06:45 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,25 @@
 // JavaScript Document
 console.log("hi");
+
+
+var shopButtons = document.querySelectorAll('main section ul li button');
+
+for (let i = 0; i < shopButtons.length; i++) {
+  shopButtons[i].onclick = addToShoppingCart;
+}
+
+function addToShoppingCart(){
+    let shoppingCartAmount = document.querySelector('header ul button span');
+
+    let currentAmount = shoppingCartAmount.innerHTML;
+    currentAmount = parseInt(currentAmount);
+  
+    let newAmount = currentAmount + 1;
+
+    shoppingCartAmount.innerHTML = newAmount;
+}
+
+/*  
+  Bronnen
+  JS - Oefening 3, link: https://codepen.io/shooft/pen/raOpRBm
+*/ 
